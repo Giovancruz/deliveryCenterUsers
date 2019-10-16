c950e96cc77e4a02ec2fcfe4b7ce2797dc4eca5f
Giovan Cruz <giovan.cruz@fh.com.br>
2019-10-16 18:18:49 -0300

    Convert to class component

--- src/components/UsersList/userListItem.jsx
+++ src/components/UsersList/userListItem.jsx
@@ -1,15 +1,16 @@
 import React, { Component } from 'react';
-import { makeStyles } from '@material-ui/core/styles';
+import { withStyles } from '@material-ui/core/styles';
 import { Paper, Typography, Avatar, Box } from '@material-ui/core';
 import Images from '../../static/media/*.jpg';
 import Icon from '@material-ui/core/Icon';
+import Grow from '@material-ui/core/Grow';
 
 
-const useStyles = makeStyles(theme => ({
+const styles = theme => ({
   card: {
     display: 'flex',
     flexDirection: 'column',
-    height: `calc(100% - ${theme.spacing(3) * 2}px)`,
+    height: '100%',
     backgroundPosition: 'top center',
     backgroundRepeat: 'no-repeat',
     backgroundSize: '360px',
@@ -28,39 +29,46 @@ const useStyles = makeStyles(theme => ({
   },
   socialItem: {
     cursor: 'pointer',
-    transition: 'all .3ms',
+    transition: 'all .3s',
     '&:hover': {
       color: theme.palette.primary.main
     }
   }
-}));
+});
 
-export default function UsersListItem(props) {
-  const classes = useStyles();
-  
-  const nameInitials = () => {
-    return (props.user.name).split(' ').map(subName => subName.charAt(0)).join('').substr(0, 2).toUpperCase();
-  }
 
-  const mediaUrl = () => {
-    const keys = Object.keys(Images);
-    const image = Images[keys[keys.length * Math.random() << 0]];
-    return image;
-  }
-  
-  return(
-    <Paper className={classes.card} style={{ backgroundImage: `url(${mediaUrl()})` }}>
-    <Avatar className={classes.thumbnail}>{nameInitials()}</Avatar>
-    <Typography variant="h6" gutterBottom={true} align="center">
-    {props.user.name} ({props.user.username})
-    </Typography>
-    <Box display="flex" className={classes.social} justifyContent="space-around">
-        <Icon className={classes.socialItem} color='disabled'>mail_outline</Icon>
-        <Icon className={classes.socialItem} color='disabled'>person_pin</Icon>
-        <Icon className={classes.socialItem} color='disabled'>phone</Icon>
-        <Icon className={classes.socialItem} color='disabled'>language</Icon>
-        <Icon className={classes.socialItem} color='disabled'>location_city</Icon>
-    </Box>
-    </Paper >
+class userListItem extends Component {
+  render() {
+    const { classes } = this.props;
+
+    const nameInitials = () => {
+      return (this.props.user.name).split(' ').map(subName => subName.charAt(0)).join('').substr(0, 2).toUpperCase();
+    }
+
+    const mediaUrl = () => {
+      const keys = Object.keys(Images);
+      const image = Images[keys[keys.length * Math.random() << 0]];
+      return image;
+    }
+
+    return (
+      <Grow in={true}>
+      <Paper className={classes.card} style={{ backgroundImage: `url(${mediaUrl()})` }}>
+      <Avatar className={classes.thumbnail}>{nameInitials()}</Avatar>
+      <Typography variant="h6" gutterBottom={true} align="center">
+      {this.props.user.name} ({this.props.user.username})
+      </Typography>
+      <Box display="flex" className={classes.social} justifyContent="space-around">
+          <Icon className={classes.socialItem} color='disabled'>mail_outline</Icon>
+          <Icon className={classes.socialItem} color='disabled'>person_pin</Icon>
+          <Icon className={classes.socialItem} color='disabled'>phone</Icon>
+          <Icon className={classes.socialItem} color='disabled'>language</Icon>
+          <Icon className={classes.socialItem} color='disabled'>location_city</Icon>
+      </Box>
+      </Paper>
+      </Grow>
     )
   }
+}
+
+export default withStyles(styles)(userListItem);
